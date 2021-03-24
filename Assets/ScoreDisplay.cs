0100aa2383def4c43f4f79f8e84314cfe8c7ad70
Sawyer893 <1707243@Mukilteo.wednet.edu>
2021-03-24 12:21:44 -0700

    First push

---
+++ Assets/ScoreDisplay.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    public Text Scoreboard;
+    public GameObject ball;
+
+    private int PaddleLeft = 0;
+    private int PaddleRIght = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ball = GameObject.Find("Ball");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= 17f)
+        {
+            PaddleLeft++;
+
+            if (transform.position.y <= -17f)
+            {
+                PaddleLeft++;
+            }
+
+        }
+
+        Scoreboard.text = PaddleLeft.ToString() + " - " + PaddleRIght.ToString();
+
+        print (PaddleLeft + " , " + PaddleRIght);
+
+    }
+}
